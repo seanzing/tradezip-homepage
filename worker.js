a10135f0120524217d96501a8d6d55ebafd4253b
Max — ZING Chief of Staff <max@zing.work>
2026-08-17 23:04:04 -0600

    Clean up - static HTML only

--- worker.js
+++
@@ -1,8 +0,0 @@
-import html from './index.html';
-export default {
-  async fetch(request) {
-    return new Response(html, {
-      headers: { 'Content-Type': 'text/html; charset=utf-8' }
-    });
-  }
-};
